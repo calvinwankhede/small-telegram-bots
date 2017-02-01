d911359f8c4a7b4b1cf5fa5167982ed58a919c68
Calvin Wankhede <calvinw1000@gmail.com>
2017-02-02 02:51:42 +0530

    First commit

---
+++ multipurpose.py
@@ -0,0 +1,133 @@
+import logging
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler
+import xkcd
+import sqlite3
+import configparser
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+config = configparser.ConfigParser()
+config.read('config.txt')
+
+xkcdexplanation = "Request an XKCD comic first with the /xkcd command"
+
+def start(bot, update):
+	msg = "Hey {username}, I'm {botname}. You can: \n\n"
+	msg += "1) Request a random XKCD with /xkcd \n"
+	msg += "2) Access our shared to-do list with /list and add items to it with /add"
+	update.message.reply_text(msg.format(username=update.message.from_user.first_name, botname=bot.name), quote=False)
+
+# Start XKCD
+def randomxkcd(bot, update):
+	update.message.reply_text("One random XKCD comic, coming right up!", quote=False)
+	randomcomic = xkcd.getRandomComic()
+	getlink = randomcomic.getImageLink()
+	comlink = str(getlink)
+	print(comlink)
+	update.message.reply_photo(comlink, caption="%s.\nReply with /xkcd_explain for an explanation."% (randomcomic.getTitle()),quote=False)
+	global xkcdexplanation
+	xkcdexplanation = str(randomcomic.getExplanation())
+	return
+
+def explanation(bot, update):
+	update.message.reply_text(xkcdexplanation, quote=False)
+# End XKCD
+
+# Start list
+def sendlist(bot, update):
+	if len(msg) == 0:
+		update.message.reply_text("Nothing has been added yet. Reply with /help if you don't know what to do.", quote=False)
+	else:
+		update.message.reply_text(msg, quote=False)
+
+def list(table):
+	global msg
+	msg = ""
+	conn = sqlite3.connect('list.db')
+	c = conn.cursor()
+	if table == "todo":
+		listitems = c.execute('SELECT item FROM todo')
+	elif table == "movies":
+		listitems = c.execute('SELECT item FROM movies')
+	else:
+		msg = "Sorry, but that list doesn't exist!"
+	i = 0
+	try:
+		for item in listitems:
+			i += 1
+			msg += "{}. {}\n".format(str(i), item[0])
+	except:
+		return
+	conn.commit()
+	conn.close()
+
+# Start add
+def additem(bot, update, args):
+	conn = sqlite3.connect('list.db')
+	c = conn.cursor()
+	if len(args) == 0:
+		update.message.reply_text("The command you entered is incomplete.", quote=False)
+	else:
+		for thing in args:
+			c.execute("INSERT INTO todo VALUES (?)", (thing,))
+		update.message.reply_text("Added successfully!\nReply with /list to see all items.", quote=False)
+	conn.commit()
+	conn.close()
+
+# Start remove
+def removeitem(bot, update, args):
+	conn = sqlite3.connect('list.db')
+	c = conn.cursor()
+	if len(args) == 0:
+		update.message.reply_text("The command you entered is incomplete.", quote=False)
+	else:
+		for thing in args:
+			c.execute("DELETE from todo where ITEM='%s';"%(thing,))
+		update.message.reply_text("Items removed successfully!", quote=False)
+	conn.commit()
+	conn.close()
+
+def removeall(bot, update, args):
+	conn = sqlite3.connect('list.db')
+	c = conn.cursor()
+	if len(args) == 0:
+		update.message.reply_text("The command you entered is incomplete.", quote=False)
+	elif args[0] == "YES":
+		c.execute("DELETE from todo")
+		update.message.reply_text("All items removed successfully! I hope you know what you just did...", quote=False)
+	else:
+		update.message.reply_text("The command you entered is incomplete.", quote=False)
+	conn.commit()
+	conn.close()
+
+
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+def main():
+	# login to Telegram with the bot token
+	updater = Updater(config['configuration']['token'])
+	dp = updater.dispatcher
+
+	# handlers for all commands that this bot supports
+	dp.add_handler(CommandHandler("start", start))
+	# XKCD
+	dp.add_handler(CommandHandler("xkcd", randomxkcd))
+	dp.add_handler(CommandHandler("xkcd_explain", explanation))
+	# list
+	dp.add_handler(CommandHandler("list", sendlist, pass_args=True))
+	dp.add_handler(CommandHandler("add", additem, pass_args=True))
+	dp.add_handler(CommandHandler("remove", removeitem, pass_args=True))
+	dp.add_handler(CommandHandler("removeall", removeall, pass_args=True))
+
+	# miscellaneous
+	dp.add_error_handler(error)
+	updater.start_polling()
+	updater.idle()
+
+if __name__ == '__main__':
+	main()
