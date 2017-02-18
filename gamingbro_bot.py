d4c9dfaca3c4cb3419f1b98ca97191f42a60bef6
Calvin Wankhede <calvinw1000@gmail.com>
2017-02-18 20:21:17 +0530

    Fully functional search, getprice and inventory worth

--- gamingbro_bot.py
+++ gamingbro_bot.py
@@ -1,55 +1,110 @@
 import telegram
-from telegram.ext import CommandHandler, Updater, RegexHandler
+from telegram.ext import CommandHandler, Updater, RegexHandler, ConversationHandler
 import re
 import requests
+import collections
 import configparser
 
 config = configparser.ConfigParser()
 config.read('/etc/config.txt')
+result = {}
+
+GETPRICE = range(1)
 
 def start(bot, update):
     msg = "Hey {username}, I'm {botname}. You can: \n\n"
-    msg += "1) Search for Counter Strike skins. Send a message that starts with 'Search for', followed by your search query.  \n"
-    msg += """2) Obtain the average price of any skin. Send a message that starts with 'Price for', followed by your search query. Please note that this feature is experimental and you'll need exact names such as 'AK-47 | Redline (Field-Tested)'
-            """
+    msg += "1) Search for all Counter Strike skins. Send a message that starts with 'Search for', followed by your search query.  \n"
+    msg += "2) Obtain the median price of any skin. Send a message that starts with 'Price for', followed by your search query."
+    msg += "3) Get the inventory worth for any Steam user. Send a message that starts with 'Inventory', followed by the Steam community username."
     update.message.reply_text(msg.format(username=update.message.from_user.first_name, botname=bot.name), quote=False)
 
 def searchfile(query):
-    found_skins = ""
+    i = 1
+    dictionary = collections.OrderedDict()
     with open("csgoitems.txt") as itemlist:
         for line in itemlist:
-            if re.search("{0}".format(query),line):
-                print(line)
-                found_skins += line
-    return found_skins
+            if query in line.lower():
+                dictionary[str(i)] = line.rstrip()
+                i += 1
+    return dictionary
 
 def searchlist(bot, update):
-    skin = update.message.text[11:]
+    skin = update.message.text[11:].lower()
     result = searchfile(skin)
+    msg = ""
     if len(result) == 0:
         update.message.reply_text("Not found", quote=False)
-    elif len(result.split("\n")) >= 40:
-        update.message.reply_text("More than 40 results found", quote=False)
     else:
-        update.message.reply_text(result, quote=False)
+        for key, value in result.items():
+            msg += "{}) {}\n".format(key, value)
+        update.message.reply_text(msg, quote=False)
 
-def price(bot, update):
-    query = update.message.text[10:]
-    params = {'id': query}
-    url = "http://csgobackpack.net/api/GetItemPrice/"
+def pricequery(bot, update):
+    skin = update.message.text[10:].lower()
+    global result
+    result = searchfile(skin)
+    msg = ""
+    if len(result) == 0:
+        update.message.reply_text("Not found", quote=False)
+    else:
+        for key, value in result.items():
+            msg += "{}) {}\n".format(key, value)
+        msg += "\nReply with the skin number for the price or cancel. For example, reply '2'."
+        update.message.reply_text(msg, quote=False)
+    return GETPRICE
+
+def getprice(bot, update):
+    number = update.message.text
+    global result
+    query = result.get(number)
+    if query != None:
+        params = {'id': query}
+        url = "http://csgobackpack.net/api/GetItemPrice"
+        r = requests.get(url, params=params)
+        output = r.json()
+        if output["success"] is True:
+            update.message.reply_text("The median price for {} is ${}.".format(query, output["median_price"]), quote=False)
+        else:
+            update.message.reply_text("An error occured, please check the name of the item or try again later.", quote=False)
+        return ConversationHandler.END
+    else:
+        update.message.reply_text("You specified an invalid number. Start over.", quote=False)
+        return ConversationHandler.END
+
+def cancel(bot, update):
+    update.message.reply_text('You know how to summon me.')
+    return ConversationHandler.END
+
+def inventory(bot, update):
+    account = update.message.text[10:]
+    params = {'id': account}
+    url = "http://csgobackpack.net/api/GetInventoryValue/"
     r = requests.get(url, params=params)
     output = r.json()
-    if output["success"] is True:
-        update.message.reply_text("The median price for {} is ${}.".format(query, output["median_price"]), quote=False)
+    if output["success"] == "true":
+        update.message.reply_text("{}'s inventory value is ${}.".format(account, output["value"]), quote=False)
     else:
-        update.message.reply_text("An error occured, please check the name of the item or try again later.", quote=False)
+        update.message.reply_text("An error occured, please check the Steam ID and if the requested profile has a public inventory.", quote=False)
+    
 
 updater = Updater(config['configuration']['gamingbro_token'])
-print(config['configuration']['gamingbro_token'])
 dp = updater.dispatcher
 
 dp.add_handler(CommandHandler("start", start))
 dp.add_handler(RegexHandler("^[Ss]earch for\s.*", searchlist))
-dp.add_handler(RegexHandler("^[Pp]rice for\s.*", price))
+
+conv_handler = ConversationHandler(
+    entry_points=[RegexHandler("^[Pp]rice for\s.*", pricequery)],
+
+    states={
+        GETPRICE: [RegexHandler("^[0-9]*", getprice)]
+    },
+
+    fallbacks=[RegexHandler("[Cc]ancel", cancel)]
+)
+dp.add_handler(conv_handler)
+
+dp.add_handler(RegexHandler("^[Ii]nventory\s.*", inventory))
+
 updater.start_polling()
 updater.idle()
