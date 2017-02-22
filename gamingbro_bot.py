1a76c3e0e81ca9a123f83b41feb00414f77d6077
Calvin Wankhede <calvinw1000@gmail.com>
2017-02-22 19:47:10 +0530

    fixed a lot

--- gamingbro_bot.py
+++ gamingbro_bot.py
@@ -11,32 +11,44 @@ config.read('/etc/config.txt')
 result = {}
 exchange_rate = 0
 
-GETPRICE = range(1)
+SENDPRICE = range(1)
 
 def start(bot, update):
     msg = "Hey {username}, I'm {botname}. You can: \n\n"
-    msg += "1) Search for all Counter Strike skins. Send a message that starts with 'Search for', followed by your search query.  \n"
-    msg += "2) Obtain the median price of any skin. Send a message that starts with 'Price for', followed by your search query.\n"
-    msg += "3) Get the inventory worth for any Steam user. Send a message that starts with 'Inventory', followed by the Steam community username."
+    msg += "1) Obtain the median price of any skin. Send a message that starts with 'Price for', followed by your search query.\n"
+    msg += "2) Get the inventory worth for any Steam user. Send a message that starts with 'Inventory', followed by the Steam community username."
     update.message.reply_text(msg.format(username=update.message.from_user.first_name, botname=bot.name), quote=False)
 
 def currencyconversion():
     url = "http://api.fixer.io/latest?base=USD"
     r = requests.get(url)
-    global available_currencies, exchange_rate
     try:
         exchange_rate = r.json()
-        available_currencies = [key for key in exchange_rate["rates"].items()]
+        with open("currencies.txt", "w") as f:
+            for currency, value in exchange_rate["rates"].items():
+                f.write("{} {}\n".format(currency, value))
     except:
         return
 
 def currencyset(bot, update, args):
-    chatid = str(update.message.chat_id)
-    update.message.reply_text("Attempting to set your chat to {}".format(args[0]), quote=False)
-    with open("currencyprefs.txt", "r") as currencyprefs:
-        for line in currencyprefs:
-            if chatid in line:
-                update.message.reply_text("Stop updating the currency, you dumbfuck", quote=False)
+    with open("currencies.txt", "r") as currencies:
+        available_currencies = [x[0:3] for x in currencies]
+    currency = args[0]
+    update.message.reply_text("Attempting to set this chat's currency to {}".format(currency.upper()), quote=False)
+    if currency.upper() in available_currencies:
+        chatid = str(update.message.chat_id)
+        with open("currencyprefs.txt", "r") as currencyprefs:
+            for line in currencyprefs:
+                if chatid in line:
+                    update.message.reply_text("You have already set your currency to {}. This cannot be changed as of now. Please contact @Sharpened if you have to.".format(line[-4:]), quote=False)
+                    return
+        with open("currencyprefs.txt", "a") as currencyprefs:
+            currencyprefs.write("{} {}\n".format(chatid, currency.upper()))
+            update.message.reply_text("Currency was changed successfully.", quote=False)
+    elif currency.upper() == "USD":
+        update.message.reply_text("The default currency was USD. Why do you want to keep changing it, huh?", quote=False)
+    else:
+        update.message.reply_text("{} is not a valid currency, please try again.\nAcceptable forms are INR or CAD for example.".format(currency.upper()), quote=False)
 
 def searchfile(query):
     i = 1
@@ -53,18 +65,20 @@ def searchfile(query):
                 i += 1
     return dictionary
 
-def searchlist(bot, update):
-    skin = update.message.text[11:].lower()
-    result = searchfile(skin)
-    msg = ""
-    if len(result) == 0:
-        update.message.reply_text("Your search returned zero results. Please try again.", quote=False)
-    elif len(result) > 50:
-        update.message.reply_text("Your search returned >50 results. Please try being more specific, for example 'AK redline' or 'AWP hyper beast'", quote=False)    
+def getprice(skin, requested_currency, currency_rate):
+    params = {'id': skin}
+    url = "http://csgobackpack.net/api/GetItemPrice"
+    r = requests.get(url, params=params)
+    output = r.json()
+    if requested_currency == "USD":
+        rounded_price = '{} {}'.format(output["median_price"], requested_currency)
+        return "The median price for {} is {}.".format(skin, rounded_price)
+    elif output["success"] is True:
+        correct_price = float(output["median_price"]) * currency_rate
+        rounded_price = '{0:,.2f}'.format(correct_price) + " " + requested_currency
+        return "The median price for {} is {}.".format(skin, rounded_price)
     else:
-        for key, value in result.items():
-            msg += "{}) {}\n".format(key, value)
-        update.message.reply_text(msg, quote=False)
+        return "Our contact with Steam's market seems to be broken due to too many requests, please try again in an hour."
 
 def pricequery(bot, update):
     skin = update.message.text[10:].lower()
@@ -90,40 +104,34 @@ def pricequery(bot, update):
         customkeyboard.append(second_row)
         reply_markup = telegram.ReplyKeyboardMarkup(customkeyboard, one_time_keyboard=True)
         update.message.reply_text(msg, quote=False, reply_markup=reply_markup, parse_mode="Markdown")
-    return GETPRICE
+    return SENDPRICE
 
-def getprice(bot, update):
+def sendprice(bot, update):
     requested_currency = "USD"
+    currency_rate = 1
     chatid = str(update.message.chat_id)
+    global result
     with open("currencyprefs.txt", "r") as currencyprefs:
         for line in currencyprefs:
             if chatid in line:
                 requested_currency = line[-4:].rstrip()
-    global result, exchange_rate
-    number = update.message.text
-    query = result.get(number)
-    reply_markup = telegram.ReplyKeyboardRemove()
+                with open("currencies.txt", "r") as currencies:
+                    for line in currencies:
+                        if requested_currency in line:
+                            currency_rate = float(line[4:].rstrip())
+    query = result.get(update.message.text)
     if query != None:
-        params = {'id': query}
-        url = "http://csgobackpack.net/api/GetItemPrice"
-        r = requests.get(url, params=params)
-        output = r.json()
-        if requested_currency == "USD":
-            rounded_price = '{} {}'.format(output["median_price"], requested_currency)
-        else:
-            correct_price = float(output["median_price"]) * exchange_rate["rates"][requested_currency]
-            rounded_price = '{0:,.2f}'.format(correct_price) + " " + requested_currency
-        if output["success"] is True:
-            update.message.reply_text("The median price for {} is {}.".format(query, rounded_price), quote=False, reply_markup=reply_markup)
-        else:
-            update.message.reply_text("Our contact with Steam's market seems to be broken due to too many requests, please try again in an hour.", quote=False, reply_markup=reply_markup)
+        reply_markup = telegram.ReplyKeyboardRemove()
+        response = getprice(query, requested_currency, currency_rate)
+        update.message.reply_text(response, quote=False, reply_markup=reply_markup)
         return ConversationHandler.END
     else:
-        update.message.reply_text("You specified an invalid number. Start over.", quote=False, reply_markup=reply_markup)
+        update.message.reply_text("You specified an invalid number. Please start the search again.", quote=False, reply_markup=reply_markup)
         return ConversationHandler.END
 
 def cancel(bot, update):
-    update.message.reply_text('You know how to summon me.')
+    reply_markup = telegram.ReplyKeyboardRemove()
+    update.message.reply_text("You know how to summon me.", quote=False, reply_markup=reply_markup)
     return ConversationHandler.END
 
 # Code for inventory check
@@ -144,16 +152,15 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(RegexHandler("^[Ss]earch for\s.*", searchlist))
 
     conv_handler = ConversationHandler(
         entry_points=[RegexHandler("^[Pp]rice for\s.*", pricequery)],
 
         states={
-            GETPRICE: [RegexHandler("^[0-9]*", getprice)]
+            SENDPRICE: [RegexHandler("^[0-9]*", sendprice)]
         },
 
-        fallbacks=[RegexHandler("[Cc]ancel", cancel)]
+        fallbacks=[CommandHandler("cancel", cancel)]
     )
     dp.add_handler(conv_handler)
 
